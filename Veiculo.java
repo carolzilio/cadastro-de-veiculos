386611551fcbe05f7bbe666407590fe9a94509c9
carolzilio <carolinerzilio@gmail.com>
2026-09-16 20:47:03 -0300

    adicionei construtor da classe Veiculo

--- Veiculo.java
+++ Veiculo.java
@@ -4,5 +4,10 @@ public class Veiculo {
     private String modelo;
     private int ano;
     private String placa;
-
-}
+    
+    public Veiculo(String marca, String modelo, int ano, String placa) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.placa = placa;
+    }
